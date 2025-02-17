bc6ac4d754644c1024aa88c79d9953fc1afde7dd
Jesús Cazalilla <jcazalilla@gmail.com>
2025-02-18 00:00:46 +0100

    Apartado A

--- app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.java
+++ app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.java
@@ -20,6 +20,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewbinding.ViewBinding;
 
 import dam.pmdm.spyrothedragon.databinding.ActivityMainBinding;
 import dam.pmdm.spyrothedragon.databinding.GuideBienvenidaBinding;
@@ -84,9 +85,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        initializeGuide();
+        initializeBienvenida();
+
 
     }
+
 
     //calcula el ancho de pantalla del dsipositivo en píxeles(float)
     private float getScreenWidth() {
@@ -98,10 +101,21 @@ public class MainActivity extends AppCompatActivity {
         return screenWidth;
     }
 
+    private void initializeBienvenida() {
+
+        getSupportActionBar().hide();//ocultamos Actionbar en la guide
+
+        bienvenidaBinding.btncomenzarGuide.setOnClickListener(this::initializeGuide);
+
+        bienvenidaBinding.guidebienvenidaLayout.setVisibility(VISIBLE);
+    }
+
 
     //inicia guia
-    private void initializeGuide() {
+    private void initializeGuide(View view) {
 
+
+        personajesBinding.exitGuide.setOnClickListener(this::onExitGuide);
         personajesBinding.nextToMundos.setOnClickListener(this::initalizeMundos);
 
         if (needToStartGuide) {
@@ -110,14 +124,17 @@ public class MainActivity extends AppCompatActivity {
             binding.navView.setFocusable(false);
             binding.navView.setEnabled(false);
 
-
+            //ocultamos layout de bienvenida
+            bienvenidaBinding.guidebienvenidaLayout.setVisibility(GONE);
             //hacemos visible la guia
             personajesBinding.guidePersonajesLayout.setVisibility(VISIBLE);
         }
+
+
         ObjectAnimator scaleX = ObjectAnimator.ofFloat(
-                personajesBinding.pulseImagePersonajes, "scaleX", 1f, 0.5f);
+                personajesBinding.pulseImage, "scaleX", 1f, 0.5f);
         ObjectAnimator scaleY = ObjectAnimator.ofFloat(
-                personajesBinding.pulseImagePersonajes, "scaleY", getScreenWidth() / 3f, .5f);
+                personajesBinding.pulseImage, "scaleY", getScreenWidth() / 3f, .5f);
         ObjectAnimator fadeIn = ObjectAnimator.ofFloat(
                 personajesBinding.textStep, "alpha", 0f, 1f);
 
@@ -135,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
                 if (needToStartGuide) {
                     super.onAnimationEnd(animation);
 
-                    personajesBinding.pulseImagePersonajes.setVisibility(VISIBLE);
+                    personajesBinding.pulseImage.setVisibility(VISIBLE);
                     personajesBinding.textStep.setVisibility(VISIBLE);
                 }
             }
@@ -144,17 +161,18 @@ public class MainActivity extends AppCompatActivity {
 
     private void initalizeMundos(View view) {
 
+        mundosBinding.nextToColeccionables.setOnClickListener(this::initalizeColeccionables);
+        mundosBinding.exitGuide.setOnClickListener(this::onExitGuide);
+
         navController.navigate(R.id.navigation_worlds);//Mostramos por debajo el fragment worlds
         personajesBinding.getRoot().setVisibility(GONE);//ocultamos guide del que viene
         mundosBinding.getRoot().setVisibility(VISIBLE);//hacemos visible la guide a la que llegamos
 
 
-        mundosBinding.nextToColeccionables.setOnClickListener(this::initalizeColeccionables);
-
         ObjectAnimator scaleX = ObjectAnimator.ofFloat(
-                mundosBinding.pulseImageMundos, "scaleX", 1f, 0.5f);
+                mundosBinding.pulseImage, "scaleX", 1f, 0.5f);
         ObjectAnimator scaleY = ObjectAnimator.ofFloat(
-                mundosBinding.pulseImageMundos, "scaleY", getScreenWidth() / 2f, .5f);
+                mundosBinding.pulseImage, "scaleY", getScreenWidth() / 2f, .5f);
         ObjectAnimator fadeIn = ObjectAnimator.ofFloat(
                 mundosBinding.textStep, "alpha", 0f, 1f);
 
@@ -164,20 +182,36 @@ public class MainActivity extends AppCompatActivity {
         animatorSet.play(scaleX).with(scaleX).before(fadeIn);
         animatorSet.setDuration(1500);
         animatorSet.start();
+
+        animatorSet.addListener((new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (needToStartGuide) {
+                    super.onAnimationEnd(animation);
+
+                    mundosBinding.pulseImage.setVisibility(VISIBLE);
+                    mundosBinding.textStep.setVisibility(VISIBLE);
+                }
+            }
+        }));
     }
 
+
     private void initalizeColeccionables(View view) {
+
+        coleccionablesBinding.nextToResumen.setOnClickListener(this::initalizeInfo);
+        coleccionablesBinding.exitGuide.setOnClickListener(this::onExitGuide);
+
 
         navController.navigate(R.id.navigation_collectibles);//mostramos el fragment collectibles
         mundosBinding.getRoot().setVisibility(GONE);//ocultamos guide del que viene
         coleccionablesBinding.getRoot().setVisibility(VISIBLE);//hacemos visible la guide a la que llegamos
 
-        //coleccionablesBinding.nextToResumen.setOnClickListener(this::initalizeInfo);
 
         ObjectAnimator scaleX = ObjectAnimator.ofFloat(
-                coleccionablesBinding.pulseImageColeccionables, "scaleX", 1f, 0.5f);
+                coleccionablesBinding.pulseImage, "scaleX", 1f, 0.5f);
         ObjectAnimator scaleY = ObjectAnimator.ofFloat(
-                coleccionablesBinding.pulseImageColeccionables, "scaleY", getScreenWidth(), .5f);
+                coleccionablesBinding.pulseImage, "scaleY", getScreenWidth(), .5f);
         ObjectAnimator fadeIn = ObjectAnimator.ofFloat(
                 coleccionablesBinding.textStep, "alpha", 0f, 1f);
 
@@ -188,14 +222,76 @@ public class MainActivity extends AppCompatActivity {
         animatorSet.setDuration(1500);
         animatorSet.start();
 
+        animatorSet.addListener((new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (needToStartGuide) {
+                    super.onAnimationEnd(animation);
+
+                    coleccionablesBinding.pulseImage.setVisibility(VISIBLE);
+                    coleccionablesBinding.textStep.setVisibility(VISIBLE);
+                }
+            }
+        }));
+
     }
 
     private void initalizeInfo(View view) {
 
+        getSupportActionBar().show();
+
+        infoBinding.nextToResumen.setOnClickListener(this::initalizeResumen);
+        infoBinding.exitGuide.setOnClickListener(this::onExitGuide);
+
+        infoBinding.getRoot().setVisibility(VISIBLE);
+        coleccionablesBinding.getRoot().setVisibility(GONE);
+
+
+
+
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(
+                infoBinding.pulseImage, "scaleX", 1f, 0.5f);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(
+                infoBinding.pulseImage, "scaleY", getScreenWidth(), .5f);
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(
+                infoBinding.textStep, "alpha", 0f, 1f);
+        scaleX.setRepeatCount(3);
+        scaleY.setRepeatCount(3);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(scaleX).with(scaleX).before(fadeIn);
+        animatorSet.setDuration(1500);
+        animatorSet.start();
+
+        animatorSet.addListener((new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (needToStartGuide) {
+                    super.onAnimationEnd(animation);
+
+                    coleccionablesBinding.pulseImage.setVisibility(VISIBLE);
+                    coleccionablesBinding.textStep.setVisibility(VISIBLE);
+                }
+            }
+        }));
+
+        showInfoDialog();
+    }
+
+    private void initalizeResumen(View view) {
+
+        getSupportActionBar().hide();
+
+        resumenBinding.startGame.setOnClickListener(this::onExitGuide);
+
+        infoBinding.getRoot().setVisibility(GONE);
+        resumenBinding.guideResumenLayout.setVisibility(VISIBLE);
     }
 
 
     private void onExitGuide(View view) {
+
+        //volvemos a mostrar ActionBar después de la guide
+        getSupportActionBar().show();
 
         needToStartGuide = false;
 
@@ -204,8 +300,12 @@ public class MainActivity extends AppCompatActivity {
         binding.navView.setFocusable(true);
         binding.navView.setEnabled(true);
 
-        //hacemos invisible la guia
-        personajesBinding.guidePersonajesLayout.setVisibility(View.INVISIBLE);
+        //hacemos invisible los layouts de la guia
+        personajesBinding.guidePersonajesLayout.setVisibility(GONE);
+        mundosBinding.guideMundosLayout.setVisibility(GONE);
+        coleccionablesBinding.guideColeccionablesLayout.setVisibility(GONE);
+        infoBinding.guideInfoLayout.setVisibility(GONE);
+        resumenBinding.guideResumenLayout.setVisibility(GONE);
     }
 
 
